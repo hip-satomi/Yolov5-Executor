667e19b90eee6a5e9af21e930bebed90511eefbc
Johannes Seiffarth <j.seiffarth@fz-juelich.de>
2022-02-22 10:08:14 +0100

    Initial commit
    - basic adaptions to yolov5
    - using code from cellpose executor

---
+++ main.py
@@ -0,0 +1,83 @@
+import numpy as np
+import os
+import sys
+import mlflow
+import json
+import torch
+from PIL import Image
+import glob
+
+
+from utils import get_git_revision_short_hash, get_git_url
+
+# get the git hash of the current commit
+short_hash = get_git_revision_short_hash()
+git_url = get_git_url()
+
+import argparse
+
+def predict(images, yolo_type='yolov5s'):
+    full_result = []
+
+    # Model
+    model = torch.hub.load('ultralytics/yolov5', yolo_type)  # or yolov5m, yolov5l, yolov5x, custom
+
+    # Inference
+    results = model(images)
+
+    for image_detections in results.xyxy:
+        image_segmentation = []
+        # extract the detections for every image
+        for det in image_detections:
+            x1,y1,x2,y2,conf,label = det.cpu().detach().numpy()
+            x1,y1,x2,y2 = map(lambda x: np.round(x).astype(int), [x1,y1,x2,y2])
+
+            image_segmentation.append(
+                dict(
+                    label = '{label}',
+                    contour_coordinates = [(x1, y1), (x2, y1), (x2, y2), (x1, y2)],
+                    type = 'Polygon'
+                )
+            )
+        
+        # collect the image result
+        image_result = dict(
+            model_version = f'{git_url}#{short_hash}',
+            format_version = '0.1',
+            segmentation = image_segmentation
+        )
+
+        # append to result list
+        full_result.append(image_result)
+
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+parser.add_argument('images', type=str, nargs='+',
+                    help='list of images')
+parser.add_argument('--yolo-type', default="yolov5s", help="Use the omnipose model")
+
+args = parser.parse_args()
+
+if len(args.images) == 1:
+    image_path = args.images[0]
+    if os.path.isdir(image_path):
+        # it's a folder, iterate all images in the folder
+        args.images = sorted(glob.glob(os.path.join(image_path, '*.png')))
+    else:
+        # it may be a list of images
+        args.images = image_path.split(' ')
+
+images = [np.asarray(Image.open(image_path)) for image_path in args.images]
+
+yolo_type = args.yolo_type
+
+result = predict(images, yolo_type)
+
+if len(images) == 1:
+    result = result[0]
+
+with open('output.json', 'w') as output:
+    json.dump(result, output)
+
+mlflow.log_artifact('output.json')
---
+++ tests/test.py
@@ -0,0 +1,69 @@
+import unittest
+import os
+
+
+class TestSegmentation(unittest.TestCase):
+
+    def setUp(self):
+        # download the image
+        import requests
+
+        url = 'https://fz-juelich.sciebo.de/s/wAXbC0MoN1G3ST7/download'
+        r = requests.get(url, allow_redirects=True)
+
+        print(len(r.content))
+
+        with open('test.png', 'wb') as file:
+            file.write(r.content)
+
+    def test_standard(self):
+        # test entrypoints: main (Yolov5s)
+        self.predict('main')
+
+    def predict(self, entrypoint):
+        import requests
+        from io import BytesIO
+        from PIL import Image
+        import json
+
+        contours = []
+
+        image = Image.open('test.png')
+
+        # convert image into a binary png stream
+        byte_io = BytesIO()
+        image.save(byte_io, "png")
+        byte_io.seek(0)
+
+        # pack this into form data
+        multipart_form_data = [
+            ("files", ("data.png", byte_io, "image/png"))
+        ]
+
+        # get job specific environment variables
+        CI_COMMIT_SHA = os.environ['CI_COMMIT_SHA']
+        CI_REPOSITORY_URL = os.environ['CI_REPOSITORY_URL']
+
+        additional_parameters = {}
+
+        # exactly request segmentation with the current repo version
+        params = dict(
+            repo=CI_REPOSITORY_URL,
+            entry_point=entrypoint,
+            version=CI_COMMIT_SHA,
+            parameters=json.dumps(additional_parameters),
+        )
+
+        # send a request to the server
+        response = requests.post(
+            'http://segserve/batch-image-prediction/', params=params, files=multipart_form_data, timeout=600
+        )
+
+        # output response
+        print(response.content)
+
+        # the request should be successful
+        self.assertTrue(response.status_code == 200)
+
+if __name__ == '__main__':
+    unittest.main()
